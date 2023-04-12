b5ec7ed0fd3a79472e5bbb7429fa07934ad78fb8
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2023-04-12 22:39:30 +0530

    Updates

---
+++ precache-manifest.cdce24d1a89caf68759c80253e070fbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "db69ca22690f59ab61b0",
+    "url": "/portofolio/static/css/main.6fc590d6.chunk.css"
+  },
+  {
+    "revision": "db69ca22690f59ab61b0",
+    "url": "/portofolio/static/js/main.db69ca22.chunk.js"
+  },
+  {
+    "revision": "04a5c51b820e1ca804da",
+    "url": "/portofolio/static/css/1.f0554b08.chunk.css"
+  },
+  {
+    "revision": "04a5c51b820e1ca804da",
+    "url": "/portofolio/static/js/1.04a5c51b.chunk.js"
+  },
+  {
+    "revision": "f9ab645a81c206d1f2da",
+    "url": "/portofolio/static/js/runtime~main.f9ab645a.js"
+  },
+  {
+    "revision": "8d7ae5ac6e934401e57e82f70b996bc1",
+    "url": "/portofolio/static/media/ab-img.8d7ae5ac.png"
+  },
+  {
+    "revision": "9bc7b3420d0e5b4bbc39eba929b865f5",
+    "url": "/portofolio/static/media/profile.9bc7b342.png"
+  },
+  {
+    "revision": "bc6ad9bd12c6a50d76584f3926f80789",
+    "url": "/portofolio/index.html"
+  }
+];
